4f3487ebfad3a9e7367627dd1472fdae2510a8e7
Jason Harris <jason@storyhug.com>
2014-07-21 13:24:00 +0100

    optimised imports and fixed readme

--- src/main/java/com/foodit/test/sample/controller/DataLoadController.java
+++ src/main/java/com/foodit/test/sample/controller/DataLoadController.java
@@ -1,24 +1,16 @@
 package com.foodit.test.sample.controller;
 
-import com.foodit.test.sample.entities.RestaurantData;
 import com.foodit.test.sample.service.RestaurantDataService;
-import com.foodit.test.sample.service.RestaurantDataServiceImpl;
 import com.google.appengine.labs.repackaged.com.google.common.collect.Lists;
-import com.google.common.io.Resources;
 import com.threewks.thundr.http.ContentType;
 import com.threewks.thundr.http.HttpSupport;
 import com.threewks.thundr.logger.Logger;
 import com.threewks.thundr.view.jsp.JspView;
 import com.threewks.thundr.view.string.StringView;
-import org.apache.commons.io.IOUtils;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.List;
-
-import static com.googlecode.objectify.ObjectifyService.ofy;
 
 public class DataLoadController {
     private final RestaurantDataService restaurantDataService;
